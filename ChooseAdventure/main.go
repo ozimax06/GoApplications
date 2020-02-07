cb6c347fdd05b1da5e1c07078a2eb050fb6ef6fb
Onder <ondero@LONKLHC02Z25QALVCF.local>
2020-02-07 00:03:09 +0000

    unmarshalled stories.json

--- ChooseAdventure/main.go
+++ ChooseAdventure/main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-type Arc struct {
-	Title string
-	Story []string
+func main() {
+	//var stories map[string]Arc
+	//fmt.Print(stories)
+
+	jsonFile, err := os.Open("stories.json")
+	// if we os.Open returns an error then handle it
+	if err != nil {
+		panic(err)
+	}
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var stories map[string]Arc
+	json.Unmarshal([]byte(byteValue), &stories)
+
+	fmt.Println(stories["new-york"].Story[3])
+
 }
 
-func main() {
+func readFromTemplate() {
 	tmpl := template.Must(template.ParseFiles("layout.html"))
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("rew")
-		data := Arc{
-			Title: "Title",
-			Story: []string{"Story1", "Story2", "Story3"},
-		}
+		data := "dddo"
 		tmpl.Execute(w, data)
 	})
 	http.ListenAndServe(":8090", nil)
+}
+
+type Arc struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []Option `json:"options"`
+}
+
+type Option struct {
+	Text string `json:"text"`
+	Arc  string `json:"arc"`
 }
