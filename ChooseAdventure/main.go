cf0754da4be0bd7bc49c8536ccc9c2a98bd274e3
Onder <ondero@LONKLHC02Z25QALVCF.local>
2020-02-09 19:28:56 +0000

    first working version. external css needs to implemented

--- ChooseAdventure/main.go
+++ ChooseAdventure/main.go
@@ -2,42 +2,59 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
-	//var stories map[string]Arc
-	//fmt.Print(stories)
 
-	jsonFile, err := os.Open("stories.json")
-	// if we os.Open returns an error then handle it
+	template := template.Must(template.ParseFiles("layout.html"))
+	stories := getStories("stories.json")
+	mux := http.NewServeMux()
+
+	sh := &StoryHandler{Stories: stories, StoryTemplate: template}
+
+	mux.Handle("/", sh)
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
+	log.Println("Listening...")
+	http.ListenAndServe(":8090", mux)
+
+}
+
+func getStories(filename string) map[string]Arc {
+	jsonFile, err := os.Open(filename)
+
 	if err != nil {
 		panic(err)
 	}
 
 	defer jsonFile.Close()
-
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
 	var stories map[string]Arc
+	byteValue, _ := ioutil.ReadAll(jsonFile)
 	json.Unmarshal([]byte(byteValue), &stories)
 
-	fmt.Println(stories["new-york"].Story[3])
-
+	return stories
 }
 
-func readFromTemplate() {
-	tmpl := template.Must(template.ParseFiles("layout.html"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("rew")
-		data := "dddo"
-		tmpl.Execute(w, data)
-	})
-	http.ListenAndServe(":8090", nil)
+func (sh *StoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	requestPath := strings.Replace(r.URL.Path, "/", "", 1)
+	_, contains := sh.Stories[requestPath]
+	data := sh.Stories["intro"]
+
+	if contains {
+		data = sh.Stories[requestPath]
+	}
+
+	sh.StoryTemplate.Execute(w, data)
+}
+
+type StoryHandler struct {
+	Stories       map[string]Arc
+	StoryTemplate *template.Template
 }
 
 type Arc struct {
