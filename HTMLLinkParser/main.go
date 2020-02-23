cf8e01deafea595a83795378a80ed35603e6a4fa
Onder <ondero@unknown.home>
2020-02-23 22:01:12 +0000

    parser in progress

--- HTMLLinkParser/main.go
+++ HTMLLinkParser/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -13,6 +14,10 @@ import (
 func main() {
 	r := getHTML("sample.html")
 	z := html.NewTokenizer(r)
+	openAnchorTags := 0
+	//innerText := ""
+	outerAnchorLink := ""
+	//var linkTexts []Link
 
 	for {
 
@@ -26,27 +31,43 @@ func main() {
 			log.Fatalf("error tokenizing HTML: %v", z.Err())
 		}
 
-		if isStartAnchor(z, tokenType) {
-			href := getAnchorLink(z)
+		if isAnchor(z) {
+			if isStartTag(tokenType) {
+				openAnchorTags++
 
-			fmt.Println(href)
+				if openAnchorTags == 1 {
+					outerAnchorLink = getAnchorLink(z)
+					fmt.Println(outerAnchorLink)
+				}
+			} else if isEndTag(tokenType) {
+				openAnchorTags--
 
+				if openAnchorTags == 0 {
+
+				}
+			}
 		}
+
 	}
 }
 
 func getAnchorLink(n *html.Tokenizer) string {
-	return getAttributeValue(n, "href")
+	result, err := getAttributeValue(n, "href")
+
+	if err != nil {
+		result = ""
+	}
+	return result
 }
 
-func getAttributeValue(n *html.Tokenizer, attributeName string) string {
+func getAttributeValue(n *html.Tokenizer, attributeName string) (string, error) {
 
 	for {
 		attr, attrVal, hasMoreAttr := n.TagAttr()
 
 		if attr != nil && attrVal != nil {
 			if strings.ToLower(string(attr)) == attributeName {
-				return string(attrVal)
+				return string(attrVal), nil
 			}
 		}
 
@@ -54,12 +75,24 @@ func getAttributeValue(n *html.Tokenizer, attributeName string) string {
 			break
 		}
 	}
-	return ""
+	return "", errors.New("Attribute couldn't be found")
 }
 
-func isStartAnchor(n *html.Tokenizer, tokenType html.TokenType) bool {
+func isAnchor(n *html.Tokenizer) bool {
 	name, _ := n.TagName()
-	return string(name) == "a" && tokenType.String() == "StartTag"
+	return string(name) == "a"
+}
+
+func isText(tokenType html.TokenType) bool {
+	return tokenType.String() == "Text"
+}
+
+func isStartTag(tokenType html.TokenType) bool {
+	return tokenType.String() == "StartTag"
+}
+
+func isEndTag(tokenType html.TokenType) bool {
+	return tokenType.String() == "EndTag"
 }
 
 func getHTML(filePath string) io.Reader {
